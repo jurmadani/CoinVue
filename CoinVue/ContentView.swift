58f2ca49e479e4c7731dd7a0a6b313aade8c9de0
Daniel Jurma <daniel@gaim-solutions.com>
2023-12-30 20:03:48 +0200

    added color themes and images in assets

--- CoinVue/ContentView.swift
+++ CoinVue/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.theme.red
+                .ignoresSafeArea()
+            
+            VStack{
+                Text("Accent Color")
+                    .foregroundStyle(Color.theme.acccent)
+                
+                Text("Secondary text Color")
+            }
         }
-        .padding()
     }
 }
 
---
+++ CoinVue/Extensions/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  CoinVue
+//
+//  Created by Daniel Jurma on 30.12.2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let acccent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
