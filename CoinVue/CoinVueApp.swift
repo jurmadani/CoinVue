4268a96fc8ec8873d83dbba5038bbf60e319aab1
Daniel Jurma <daniel@gaim-solutions.com>
2023-12-30 19:48:33 +0200

    created Xcode project

---
+++ CoinVue/CoinVueApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinVueApp.swift
+//  CoinVue
+//
+//  Created by Daniel Jurma on 30.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoinVueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
